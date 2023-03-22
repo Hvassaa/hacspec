692eed377bf8ffda25e792a8c3de5338cd92f650
Rasmus T. Bjerg <rs.bjerg@gmail.com>
2023-03-22 12:33:41 +0000

    real poly

--- examples/halo2/src/halo2.rs
+++ examples/halo2/src/halo2.rs
@@ -1,6 +1,5 @@
 use hacspec_lib::*;
 
-
 public_nat_mod!( //Custom Macro - defining a newtype with some functions - well defined macro's have library functions built in
     type_name: Fp,
     type_of_canvas: FpCanvas,
@@ -8,53 +7,33 @@ public_nat_mod!( //Custom Macro - defining a newtype with some functions - well 
     modulo_value: "40000000000000000000000000000000224698fc094cf91b992d30ed00000001" //0x40000000000000000000000000000000224698fc094cf91b992d30ed00000001
 );
 
-public_nat_mod!( //Custom Macro - defining a new type with some functions - well defined macro's have library functions built in
-    type_name: Scalar,
-    type_of_canvas: ScalarCanvas,
-    bit_size_of_field: 256,
-    modulo_value: "8000000000000000000000000000000000000000000000000000000000000000" //0x8000000000000000000000000000000000000000000000000000000000000000
-);
+fn add_polyx(p1: Seq<Fp>, p2: Seq<Fp>) -> Seq<Fp> {
+    let mut res;
+    let short_len;
 
-// univariate polynomial represented as coefficients
-// (left-most is lowest power, right-most is highest power)
-#[derive(Clone, Debug)]
-pub struct UniPolynomial(Seq<Fp>);
+    if p1.len() > p2.len() {
+        res = p1.clone();
+        short_len = p2.len();
+    } else {
+        res = p2.clone();
+        short_len = p1.len();
+    }
 
-// evaluate polynomial "p" at point "x"
-pub fn eval_uni_poly(p: UniPolynomial, x: Fp) -> Fp {
-    let mut res: Fp = Fp::from_literal(0);
-    let UniPolynomial(s) = p;
-    s.iter().enumerate().for_each(|(i, c)| {
-        let power = i as u32;
-        res = res + (x.exp(power) * (*c));
-    });
+    for i in 0..short_len {
+        res[i] = p1[i] + p2[i];
+    }
 
     res
 }
 
-// multiply polynomial "p" by scalar "x"
-pub fn mult_uni_poly_scalar(p: UniPolynomial, x: Fp) -> UniPolynomial {
-    let UniPolynomial(s) = p;
-    let mut res = s.clone();
+fn mul_scalar_polyx(p1: Seq<Fp>, s: Fp) -> Seq<Fp> {
+    let mut res = p1.clone();
 
-    s.iter().enumerate().for_each(|(i, _)| {
-        res[i] = res[i] * x;
-    });
+    for i in 0..res.len() {
+        res[i] = p1[i] * s;
+    }
 
-    UniPolynomial(res)
-}
-
-// add polynomial "p" and polynomial "q"
-pub fn add_uni_polys(p: UniPolynomial, q: UniPolynomial) -> UniPolynomial {
-    let UniPolynomial(s1) = p;
-    let UniPolynomial(s2) = q;
-    let mut res = s1.clone();
-
-    s1.iter().enumerate().for_each(|(i, _)| {
-        res[i] = s1[i] + s2[i];
-    });
-
-    UniPolynomial(res)
+    res
 }
 
 #[cfg(test)]
@@ -69,55 +48,34 @@ extern crate polynomial;
 use quickcheck::*;
 
 #[cfg(test)]
-impl Arbitrary for UniPolynomial {
-    fn arbitrary(g: &mut quickcheck::Gen) -> UniPolynomial {
-        let size = u8::arbitrary(g) % 20;
-        let mut v = vec![];
-        for _ in 0..size {
-            v.push(Fp::from_literal(u128::arbitrary(g)));
-        }
-        UniPolynomial(Seq::<Fp>::from_vec(v))
-    }
-}
+#[quickcheck]
+fn test_poly_add() -> bool {
+    let v1 = vec![5, 10, 20]
+        .iter()
+        .map(|e| Fp::from_literal((*e) as u128))
+        .collect();
+    let v2 = vec![55]
+        .iter()
+        .map(|e| Fp::from_literal((*e) as u128))
+        .collect();
+    let p1 = Seq::from_vec(v1);
+    let p2 = Seq::from_vec(v2);
 
- 
-// polynomial for testing
-#[cfg(test)]
-fn get_test_poly() -> UniPolynomial {
-    let v = vec![5, 200, 59, 33, 0, 19];
-    let vfp: Vec<Fp> = v.iter().map(|e| Fp::from_literal(*e)).collect();
-    let s = Seq::from_vec(vfp);
+    let p3 = add_polyx(p1, p2);
 
-    UniPolynomial(s)
+    p3[0] == Fp::from_literal(60) && p3[1] == Fp::from_literal(10) && p3[2] == Fp::from_literal(20)
 }
-//
-// #[cfg(test)]
-// #[quickcheck]
-// fn test_eval_polynomial() -> bool {
-//     let p = get_test_poly();
-//
-//     eval_uni_poly(p, Fp::TWO()) == Fp::from_literal(1513) // trust me
-// }
 
 #[cfg(test)]
 #[quickcheck]
-fn test_eval_polylol() -> bool {
-    let UniPolynomial(p1) = get_test_poly();
-    let UniPolynomial(p2) = get_test_poly();
-    add_poly(&p1, &p2);
-    println!("{}", 100);
-    true
+fn test_poly_mul() -> bool {
+    let v1 = vec![5, 10, 20]
+        .iter()
+        .map(|e| Fp::from_literal((*e) as u128))
+        .collect();
+    let p1 = Seq::from_vec(v1);
+
+    let p3 = mul_scalar_polyx(p1, Fp::TWO());
+
+    p3[0] == Fp::from_literal(10) && p3[1] == Fp::from_literal(20) && p3[2] == Fp::from_literal(40)
 }
-
-
-// #[cfg(test)]
-// #[quickcheck]
-// fn test_eval_polynomial(a: UniPolynomial) -> bool {
-//     let point = Fp::from_literal(7);
-//     let a2 = a.clone();
-//     let UniPolynomial(s) = a;
-//     let p = polynomial::Polynomial::new(s.native_slice().to_vec());
-//     let home = eval_uni_poly(a2, point);
-//     let away = p.eval(point);
-//     home == away
-// }
